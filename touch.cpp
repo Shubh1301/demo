8f29b17a0546724bccb8048e22437b3feab6573a
shubh <1shubhagarwal1@gmail.com>
2021-12-03 19:13:44 +0530

    new files added

---
+++ touch.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int sum(int a, int b){
+	int sum;
+	sum = a + b;
+	return sum;
+}
+
+int main(){
+  int a=5;
+  int b=6;
+  int summ;
+
+  summ = sum(a,b);
+
+  cout<<summ<<endl;
+
+  return 0;
+}
+
+
+  
+
+
+
